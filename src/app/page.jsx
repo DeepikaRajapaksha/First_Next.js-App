cdb3bcbcc9fa3d09303d4f089112835a234a29ca
DeepikaRajapaksha <rpdsewwandi@std.appsc.sab.ac.lk>
2025-02-24 14:42:03 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+    <h1>Hello World</h1>
+    <h2>Good Luck</h2>
+    </>
+    
+  )
+}
