c03338c29850d0aeae14fb13424a25097c422d68
DeepikaRajapaksha <rpdsewwandi@std.appsc.sab.ac.lk>
2025-02-26 21:15:36 +0530

    Add CSS file

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Link from 'next/link'
+import styles from './contact.module.css'
 function page() {
   return (
+    <>
     <h1>Contact us page</h1>
+    <Link href="/"  className={styles.Link}>Home</Link>
+    
+    </>
+    
   )
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,15 +7,15 @@ export default function Home() {
     <>
     <h1>Home Page</h1>
     <ul>
-      <li><Link href="/">Home</Link></li>
-      <li><Link href="/services">Services</Link></li>
-      <li><Link href="/products">Products</Link></li>
-      <li><Link href="/about/carrers">Carrers Page </Link></li>
-      <li><Link href="/contact">Contact us</Link></li>
+      <li><Link href="/" className={styles.Link}>Home</Link></li>
+      <li><Link href="/services" className={styles.Link}>Services</Link></li>
+      <li><Link href="/products" className={styles.Link}>Products</Link></li>
+      <li><Link href="/about/carrers" className={styles.Link}>Carrers Page </Link></li>
+      <li><Link href="/contact" className={styles.Link}>Contact us</Link></li>
 
-      <li><Link href="https://nextjs.org/" target="_blank">NextJS Site</Link></li>
+      <li><Link href="https://nextjs.org/" target="_blank" id={styles.specialLink}>NextJS Site</Link></li>
 
-      <li><Link href="/products/vegis">Vegis Page</Link></li>
+      <li><Link href="/products/vegis" className={styles.Link}>Vegis Page</Link></li>
     </ul>
     </>
     
