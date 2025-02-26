b619873f0036b8ab08da7b3563e4eee28d4b4fb7
DeepikaRajapaksha <rpdsewwandi@std.appsc.sab.ac.lk>
2025-02-26 18:16:42 +0530

    Dynamic page navigation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link"
 
 export default function Home() {
   return (
     <>
     <h1>Home Page</h1>
+    <ul>
+      <li><Link href="/">Home</Link></li>
+      <li><Link href="/services">Services</Link></li>
+      <li><Link href="/products">Products</Link></li>
+      <li><Link href="/about/carrers">Carrers Page </Link></li>
+      <li><Link href="/contact">Contact us</Link></li>
+
+      <li><Link href="https://nextjs.org/" target="_blank">NextJS Site</Link></li>
+
+      <li><Link href="/products/vegis">Vegis Page</Link></li>
+    </ul>
     </>
     
   )
--- src/app/products/vegis/page.jsx
+++ src/app/products/vegis/page.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import Link from 'next/link'
 
-function page() {
+function Page() {
+
+  const vegis = [
+    {
+      id: 1,
+      veginame : "Broccoli"
+    },
+    {
+      id: 2,
+      veginame : "Potato"
+    },
+    {
+      id: 3,
+      veginame : "Carrort"
+    },
+    {
+      id: 4,
+      veginame : "Pumpkin"
+    },
+    {
+      id: 5,
+      veginame : "Spinish"
+    },
+  ];
+
   return (
-    <div>Vegis page</div>
-  )
+    <>
+      <div>Vegis page</div>
+
+      <ul>
+        {vegis.map((vegi) => (
+          <li key={vegi.id}>
+            <Link href={`/products/vegis/${vegi.veginame}`}>{vegi.veginame}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
-export default page
+export default Page;
